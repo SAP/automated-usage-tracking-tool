fb99fb9d05b92ca19ca3ebcd8da3b5f1b4a20213
Bruno Santos <b.santos@sap.com>
2024-06-28 18:20:15 +0100

    AUTT-0: Change checkmarx preset. Ignore test files on coverage. Add filter pattern to checkmarx config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 
 export default defineConfig({
   test: {
     coverage: {
       reporter: ['text', 'lcov'],
       reportsDirectory: '../coverage',
+      exclude: [...configDefaults.exclude, '**/*.test.ts', '**/*.dataTest.ts'],
     },
   },
 })
