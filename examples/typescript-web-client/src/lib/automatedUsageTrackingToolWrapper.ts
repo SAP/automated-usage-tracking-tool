d646bf8b1c35bca289bf8924385b44671823f289
Luís Marques <l.marques@sap.com>
2024-08-12 11:07:11 +0100

    Improve examples code and comments

--- examples/typescript-web-client/src/lib/automatedUsageTrackingToolWrapper.ts
+++ examples/typescript-web-client/src/lib/automatedUsageTrackingToolWrapper.ts
@@ -1,4 +1,4 @@
-import CliTracker, { TrackerArguments, ConsentArguments, TrackUsageArguments } from '@sap_oss/automated-usage-tracking-tool'
+import CliTracker, { ConsentArguments, TrackerArguments, TrackUsageArguments } from '@sap_oss/automated-usage-tracking-tool'
 import '@sap_oss/automated-usage-tracking-tool/theme/sap_horizon.css'
 
 const trackerArguments: TrackerArguments = { apiKey: '4_TCuGT23_GS-FxSIFf3YNdQ', dataCenter: 'eu1' }
@@ -10,14 +10,19 @@ const trackUsageArguments: TrackUsageArguments = { toolName: 'typescript-cli-cli
 const trackingTool = new CliTracker(trackerArguments)
 
 export async function requestConsentConfirmation() {
+  // Request anonymous consent with a custom message
   return await trackingTool.requestConsentConfirmation({
-    message: 'This is a customized request consent confirmation message.', // generated email with customized message
+    message: `
+      <h2>Application Title</h2>
+      This app collects anonymous usage data to help deliver and improve this product. By installing this app, you agree to share this information with SAP. If you wish to revoke your consent, please uninstall the app. Do you want to continue?
+    `,
   })
 }
 
 export async function requestConsentQuestion() {
+  // Request consent for a specific email
   return await trackingTool.requestConsentQuestion({
-    email: 'example@test.com', // specific email with default message
+    email: 'example@test.com',
   })
 }
 
