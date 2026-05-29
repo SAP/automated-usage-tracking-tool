780d3b09eace0852e4a1e36638f9c1fd9e1a594d
Tiago Gaspar <tiago.gaspar@sap.com>
2026-05-29 08:54:27 +0100

    enableAOA- remove unnecessary exports

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,2 @@
 export { default } from './cli'
 export { TrackerArguments, ConsentArguments, TrackUsageArguments } from '../common/tracker'
-export { TrackingReport, AOAConfig } from '../aoa/aoaClient'
-export { ToolDefinition, getToolByName } from '../aoa/toolRegistry'
--- src/web/index.ts
+++ src/web/index.ts
@@ -1,4 +1,2 @@
 export { default } from './web'
 export { TrackerArguments, ConsentArguments, TrackUsageArguments } from '../common/tracker'
-export { TrackingReport, AOAConfig } from '../aoa/aoaClient'
-export { ToolDefinition, getToolByName } from '../aoa/toolRegistry'
