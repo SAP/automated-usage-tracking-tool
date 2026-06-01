e8a599fd75cece1dadcdf24bec65805fb32d96bc
Tiago Gaspar <tiago.gaspar@sap.com>
2026-06-01 10:04:55 +0100

    enableAOA- increase package versions and clean dist folder

--- dist/cli/cliAoaClient.d.ts
+++
@@ -1,5 +0,0 @@
-import AOAClient from '../aoa/aoaClient';
-export default class CliAoaClient extends AOAClient {
-    constructor();
-    static create(): CliAoaClient | null;
-}
--- dist/web/webAoaClient.d.ts
+++
@@ -1,5 +0,0 @@
-import AOAClient from '../aoa/aoaClient';
-export default class WebAoaClient extends AOAClient {
-    constructor();
-    static create(): WebAoaClient | null;
-}
